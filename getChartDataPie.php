9e95c83e8adfcf18ae3341dde1aa3a5e3f6c5239
MarkoM91 <markomedic91@gmail.com>
2019-04-30 14:48:39 +0200

    commit4

--- getChartDataPie.php
+++ getChartDataPie.php
@@ -7,10 +7,31 @@
 
   foreach ($graphs["fatturato_by_agent"] as $value) {
 
-   $type[] = $value["type"];
-   foreach ($value["data"] as $elem) {
-     $salesmen[] = $elem[""]
+   $pie[] = $value["type"];
+   foreach ($value as $k1 => $v1) {
+
+     $salesmen[] = $k1;
+     $sales[] = $v1;
    }
   }
-var_dump($value["data"]);
+
+  $ret2 = [
+
+    "type" => $type2,
+
+    "data" => $salesmen
+        "labels" => $pie,
+        "datasets" => [
+          [
+            "label" => 'Income',
+            "backgroundColor" => ['red', 'blue',  'yellow'],
+            "borderColor" => ['red', 'blue', 'yellow'],
+            "data" => $sales
+          ]
+        ]
+    ]
+  ];
+
+  echo json_encode($ret);
+
  ?>
