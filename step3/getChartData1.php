e5cef9e02f39b387db8b4baddb1bee3734a1f112
MarkoM91 <markomedic91@gmail.com>
2019-05-02 01:42:21 +0200

    commit16

--- step3/getChartData1.php
+++ step3/getChartData1.php
@@ -8,19 +8,56 @@
   $data = [];
   $access = [];
 
-  foreach ($graphs["fatturato"] as $value) {
+  foreach ($graphs["fatturato"] as $va) {
 
-    foreach ($value as $k1 => $v1) {
+    foreach ($va as $k1a => $v1a) {
 
-      $type[] = $v1["type"];
-      $data[] = $v1["data"] ;
-      $access[] = $v1["access"];
+      $type[] = $v1a["type"];
+      $dataa[] = $v1a["data"] ;
+      $guest[] = $v1a["access"];
     }
+  }
 
+  foreach ($graphs["fatturato_by_agent"] as $vb) {
+
+    foreach ($vb as $k1b => $v1b) {
+
+      $type[] = $v1b["type"];
+      $datab[] = $v1b["data"] ;
+      $employee[] = $v1b["access"];
+    }
+  }
+
+  foreach ($graphs["team_efficiency"] as $valuec
+    foreach ($vc as $k1c => $v1c) {
+
+      $type[] = $v1c["type"];
+      $datac[] = $v1c["data"] ;
+      $clevel[] = $v1c["access"];
+    }
   }
 
 
 if ($level_guest == "guest") {
+
+  $ret = [
+
+   "type" => $type,
+
+   "data" => [
+       "labels" => ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+       "datasets" => [
+         [
+           "label" => 'Month Sales',
+           "backgroundColor" => ['red'],
+           "borderColor" => ['blue'],
+           "data" => $dataa
+         ]
+       ]
+   ]
+  ];
+
+
 
 } elseif ($level_guest == "employee") {
 
